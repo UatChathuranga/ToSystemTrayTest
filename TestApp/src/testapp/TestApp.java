6075a614645a07cc686e37a7d07d7b1cfee01f56
Thilina Chathuranga <uatchathuranga@gmail.com>
2019-09-19 22:18:23 +0530

    NB_PROJECT

---
+++ TestApp/src/testapp/TestApp.java
@@ -0,0 +1,23 @@
+/*
+ * Developer   : U.A.T.Chathuranga.
+ * Contact     : uatchathuranga@flogatech.com / uatchathuranga@gmail.com
+ * www.flogatech.com - contact@flogatech.com
+ * © CopyRight @ FLOGATechnologies - 2019. All Rights Received ®. 
+ */
+
+package testapp;
+
+/**
+ *
+ * @author uatChathuranga
+ */
+public class TestApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
